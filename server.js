c1f775eda89f635fdcd4784567c32533f46f691d
github username <github email address>
2020-05-07 21:06:14 -0400

    created GET, POST, and DELETE requests

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const express = require("express");
-const htmlRoutes = require("./routes/htmlRoute");
-const apiRoutes = require("./routes/apiRoute");
 
 const app = express();
 const PORT = process.env.PORT || 8088;
@@ -10,5 +8,27 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
+
+
+const router =require("express").Router();
+const notes = require("../db/notes.js");
+
+
+router.get("/notes", function(req, res){
+    notes.getNotes()
+    .then(notes => res.json(notes))
+    .catch(err => res.status(500).json(err));
+})
+router.post("/notes", function(req, res){
+    notes.addNotes(req.body)
+    .then(notes => res.json(notes))
+    .catch(err => res.status(500).json(err));
+})
+router.delete("/notes/:id", function(req, res){
+    notes.removeNote(req.params.id)
+    .then(() => res.json(true))
+    .catch(err => res.status(500).json(err));
+})
+
 
 app.listen(PORT,()=> console.log("Listening on PORT : " + PORT));
